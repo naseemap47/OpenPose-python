840d27967fafa9d56606ccb8db39faa1c99858f1
NASEEM A P <naseemalassampattil@gmail.com>
2023-01-17 05:39:51 +0000

    draw bbox arround Head using ear keypoints

--- examples/tutorial_api_python/01_body_from_image.py
+++ examples/tutorial_api_python/01_body_from_image.py
@@ -14,31 +14,31 @@ import cv2
 try:
     # Import Openpose (Windows/Ubuntu/OSX)
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    try:
-        # Windows Import
-        if platform == "win32":
-            # Change these variables to point to the correct folder (Release/x64 etc.)
-            sys.path.append(dir_path + '/../../python/openpose/Release');
-            os.environ['PATH']  = os.environ['PATH'] + ';' + dir_path + '/../../x64/Release;' +  dir_path + '/../../bin;'
+    # try:
+    #     # Windows Import
+    #     if platform == "win32":
+    #         # Change these variables to point to the correct folder (Release/x64 etc.)
+    #         sys.path.append(dir_path + '/../../python/openpose/Release');
+    #         os.environ['PATH']  = os.environ['PATH'] + ';' + dir_path + '/../../x64/Release;' +  dir_path + '/../../bin;'
             
-        else:
-            # Change these variables to point to the correct folder (Release/x64 etc.)
-            sys.path.append('../../python');
-            # If you run `make install` (default path is `/usr/local/python` for Ubuntu), you can also access the OpenPose/python module from there. This will install OpenPose and the python library at your desired installation path. Ensure that this is in your python path in order to use it.
-            # sys.path.append('/usr/local/python')
-            # from openpose import pyopenpose as op
-    except ImportError as e:
-        print('Error: OpenPose library could not be found. Did you enable `BUILD_PYTHON` in CMake and have this Python script in the right folder?')
-        raise e
+    #     else:
+    #         # Change these variables to point to the correct folder (Release/x64 etc.)
+    #         sys.path.append('../../python');
+    #         # If you run `make install` (default path is `/usr/local/python` for Ubuntu), you can also access the OpenPose/python module from there. This will install OpenPose and the python library at your desired installation path. Ensure that this is in your python path in order to use it.
+    #         # sys.path.append('/usr/local/python')
+    #         # from openpose import pyopenpose as op
+    # except ImportError as e:
+    #     print('Error: OpenPose library could not be found. Did you enable `BUILD_PYTHON` in CMake and have this Python script in the right folder?')
+    #     raise e
 
     # Flags
     parser = argparse.ArgumentParser()
-    parser.add_argument("--image_path", default="../../examples/media/COCO_val2014_000000000192.jpg", help="Process an image. Read all standard formats (jpg, png, bmp, etc.).")
+    parser.add_argument("--image_path", default="../../examples/media/test.jpg", help="Process an image. Read all standard formats (jpg, png, bmp, etc.).")
     args = parser.parse_known_args()
 
     # Custom Params (refer to include/openpose/flags.hpp for more parameters)
     params = dict()
-    params["model_folder"] = "../../models/"
+    params["model_folder"] = "/openpose/models"
 
     # Add others in path?
     for i in range(0, len(args[1])):
@@ -74,42 +74,67 @@ try:
 
         # Each person
         n1_x, n1_y, n2_x, n2_y = 0, 0, 0, 0
+        norm_dist = 0
         
         # Head
         head1_x, head1_y, head2_x, head2_y = 0, 0, 0, 0
 
         for id, key_id in enumerate(person):
             # print(f'Id: {id}, Key: {key_id}')
-            if key_id[2] > 0.7:
+            # if key_id[2] > 0.2:
                 
-                # Normalize
-                if id == 1:
-                    n1_x, n1_y = key_id[0], key_id[1]
-                if id == 8:    
-                    n2_x, n2_y = key_id[0], key_id[1]
-                
-                # Head
-                if id == 17:
-                    head1_x, head1_y = key_id[0], key_id[1]
-                if id == 18:    
-                    head2_x, head2_y = key_id[0], key_id[1]
+            # Normalize
+            if id == 9:
+                n1_x, n1_y = key_id[0], key_id[1]
+            if id == 12:    
+                n2_x, n2_y = key_id[0], key_id[1]
             
-        norm_dist = np.sqrt(((n1_x-n2_x)**2 + (n1_y-n2_y)**2))
-        print('Norm Dist: ', norm_dist)
+            # Head
+            if id == 17:
+                head1_x, head1_y = key_id[0], key_id[1]
+            if id == 18:    
+                head2_x, head2_y = key_id[0], key_id[1]
+            
+        if n1_x > 0 and n1_y > 0 and n2_x > 0 and n2_y > 0:
+            norm_dist = np.sqrt(((n1_x-n2_x)**2 + (n1_y-n2_y)**2))
+            print('Norm Dist: ', norm_dist)
 
-        # Head Cap
-        head_dist = np.sqrt(((head1_x-head2_x)**2 + (head1_y-head2_y)**2))
-        print('Head Dist: ', head_dist)
-        thresh_head = head_dist/norm_dist
-        print('Thresh: ', thresh_head)
+        if norm_dist > 0 and head1_x > 0 and head1_y > 0 and head2_x > 0 and head2_y > 0:
+            # Head Cap
+            head_dist = np.sqrt(((head1_x-head2_x)**2 + (head1_y-head2_y)**2))
+            print('Head Dist: ', head_dist)
+            thresh_head = head_dist/norm_dist
+            print('Thresh: ', thresh_head)
 
-        # cv2.rectangle(
-        #     datum.cvOutputData, ()
+            # print((int(thresh_head*10), int(thresh_head*2)))
+            # print(int(head1_x-thresh_head*2), int(head1_y+thresh_head*2))
+
+
+        # Draw
+        # cv2.circle(
+        #     imageToProcess, (int((head2_x-thresh_head*10), int(head2_y-thresh_head*2))),
+        #     5, (0, 255, 0), cv2.FILLED
         # )
+        # cv2.circle(
+        #     imageToProcess, (head2_x, head2_y),
+        #     5, (255, 255, 0), cv2.FILLED
+        # )
+
+            ## BBox - ROI
+            # Head ROI
+            cv2.rectangle(
+                imageToProcess, 
+                (int(head2_x-thresh_head*25), int(head2_y-thresh_head*30)),
+                (int(head1_x+thresh_head*30), int(head1_y+thresh_head*20)),
+                (0, 255, 0), 2
+            )
 
     # Display Image
     # print("Body keypoints: \n" + str(datum.poseKeypoints))
+    
     cv2.imshow("OpenPose 1.5.1 - Tutorial Python API", datum.cvOutputData)
+    cv2.imshow("img", imageToProcess)
+
     if cv2.waitKey(0) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
 except Exception as e:
